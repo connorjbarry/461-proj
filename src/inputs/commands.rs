ea271e7eafde63f87aa5e42714e3f21056b3c93a
SeanoChang <89418312+SeanoChang@users.noreply.github.com>
2023-01-29 15:52:15 -0500

    update ./run build

--- src/inputs/commands.rs
+++ src/inputs/commands.rs
@@ -37,8 +37,12 @@ impl Commands {
             .expect("failed to execute process")
     }
 
-    pub fn build(&self) -> Option<bool> {
-        self.build
+    pub fn build(&self) {
+        // build 
+        Command::new("cargo")
+            .arg("build")
+            .spawn()
+            .expect("failed to build")
     }
 
     pub fn url(&self) -> Option<String> {
